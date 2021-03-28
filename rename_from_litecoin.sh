9dc94f25951a99019e359317856050fa46691772
Jacob Cooper <jcooper036@gmail.com>
2021-03-28 09:17:09 -0600

    adds renaming script

---
+++ rename_from_litecoin.sh
@@ -0,0 +1,8 @@
+find ./ -type f -readable -writable -exec sed -i "s/Garrycoin/Garrycoin/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/GarryCoin/GarryCoin/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/BWA/BWA/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/garrycoin/garrycoin/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/garrycoind/garrycoind/g" {} \;
+
+find ./ -type f -readable -writable -exec sed -i "s/bwwwaaas/bwwwaaas/g" {} \;
+find ./ -type f -readable -writable -exec sed -i "s/rukiddings/rukiddings/g" {} \;
